fc35ca31da9633047584d2d93ccb3f8c6ed852b5
Dario Dentale <dario.dentale@digitaliasistemi.it>
2018-08-15 20:00:25 +0200

    wow

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,18 @@
 $(function() {
+
+  new WOW().init();
   
+  // GDPR
   if(!Cookies.get('gdpr')) {
     setTimeout(function () { $('#gdpr').addClass('show'); }, 2000);
   }
-
   $('#gdpr').on('closed.bs.alert', function () {
     Cookies.set('gdpr', 'true', { expires: 365 });
   })
+
+  // ANIMATIONS
+  // on scroll check element if is to be animated
+
 
 })
 
